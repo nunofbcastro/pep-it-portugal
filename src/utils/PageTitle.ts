99e862c5a42b1cfdd36be6d0164632b51f70a7ca
NUNO DE FIGUEIREDO BRITO E CASTRO <8200591@estg.ipp.pt>
2023-02-26 15:02:08 +0000

    Fixed error when changing page title

--- src/utils/PageTitle.ts
+++ src/utils/PageTitle.ts
@@ -2,10 +2,9 @@ import { useEffect } from 'react';
 
 export function useTitle(title: string) {
   useEffect(() => {
-    let newTitle = `${title.length > 0 ? `${title} - ` : ''}`;
-    console.log(newTitle);
-
     return () => {
+      let newTitle = `${title.length > 0 ? `${title} - ` : ''}`;
+      console.log(newTitle);
       document.title = `${newTitle}PorqueEUprogramo`;
     };
   }, []);

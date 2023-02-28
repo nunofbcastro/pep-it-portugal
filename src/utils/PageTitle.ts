27ac2851d87d6d5ee2fbf97686fd549ee77eae88
NUNO DE FIGUEIREDO BRITO E CASTRO <8200591@estg.ipp.pt>
2023-02-28 14:50:44 +0000

    Fixed sorting method and resolved layout errors.

--- src/utils/PageTitle.ts
+++ src/utils/PageTitle.ts
@@ -2,9 +2,6 @@ import { useEffect } from 'react';
 
 export function useTitle(title: string) {
   useEffect(() => {
-    let newTitle = `${
-      title.length > 0 ? `${title} - PorqueEUprogramo` : 'PorqueEUprogramo'
-    }`;
-    document.title = `${newTitle}`;
+    document.title = `${title.length > 0 && `${title} - `}PorqueEUprogramo`;
   });
 }
